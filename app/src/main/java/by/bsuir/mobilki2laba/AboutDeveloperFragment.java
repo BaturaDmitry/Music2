5faef3d7c52b7b0aa3f67d9860f65a361da55eb4
BaturaDmitry <koonnot@mail.ru>
2022-03-17 22:40:33 +0300

    initialize

--- app/src/main/java/by/bsuir/mobilki2laba/AboutDeveloperFragment.java
+++ app/src/main/java/by/bsuir/mobilki2laba/AboutDeveloperFragment.java
@@ -1,17 +1,14 @@
 package by.bsuir.mobilki2laba;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import by.bsuir.mobilki2laba.R;
+import androidx.fragment.app.Fragment;
 
 public class AboutDeveloperFragment extends Fragment {
-
+private String some;
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
